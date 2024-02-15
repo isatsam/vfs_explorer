0187b3de490dba354c9c9912ffb783513cbd5580
isatsam <=>
2024-02-16 01:24:36 +0300

    Fix imports in config.py

--- plaguevfs/core/config.py
+++ plaguevfs/core/config.py
@@ -2,9 +2,9 @@
 # 'The canonical way to share information across modules within a single program is to create a special module
 # (often called config or cfg). <...> Note that using a module is also the basis for implementing
 # the singleton design pattern.'
-from indexer import Indexer
-from unpacker import Unpacker
-from searcher import Searcher
+from plaguevfs.core.indexer import Indexer
+from plaguevfs.core.unpacker import Unpacker
+from plaguevfs.core.searcher import Searcher
 
 index = Indexer()
 unpack = Unpacker()
