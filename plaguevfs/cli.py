d5737352b1eddbf1ea1936b9b7b3c55302a141bf
isatsam <=>
2024-02-16 01:22:58 +0300

    Rough rebase from main

--- plaguevfs/cli.py
+++ plaguevfs/cli.py
@@ -1,7 +1,7 @@
 import argparse as arg
 from core import config
-from core.vfs import Vfs
-from core.vfs_error import VfsError
+from plaguevfs.core.vfs import Vfs
+from plaguevfs.core.vfs_error import VfsError
 
 
 if __name__ == "__main__":
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     if args.search:
         results = config.search.search(archive, args.search)
         for file in results:
-            print(f"{file[0]}     type: {file[1].file_type}")
+            print(f"{file[0]}     type: {file[1].file_type_full_name}")
 
     if args.extract:
         config.unpack.unpack(archive, args.extract)
