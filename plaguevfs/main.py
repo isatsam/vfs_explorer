c07ebf45bac152fa45ec515e7910ab9ebcdf995a
isatsam <=>
2024-02-19 23:06:59 +0300

    Fix imports, make mainWindow the one with the tree

--- plaguevfs/main.py
+++ plaguevfs/main.py
@@ -1,16 +1,16 @@
 from PyQt6.QtWidgets import QApplication
-import core.shared as common
+import core.config as config
 from core.vfs import Vfs
 from core.vfs_error import VfsError
 import ui.ui as ui
 import sys
 
-def main():
+
+def main(path_to_archive):
     app = QApplication([])
-    mainWindow = ui.UI()
 
     # Open and create archive
-    OPEN_ARCHIVE = '/home/isa/Projects/plaguevfs/archives/Textures.vfs'
+    OPEN_ARCHIVE = path_to_archive
     try:
         archive = Vfs(OPEN_ARCHIVE)
     except (VfsError, FileNotFoundError) as er:
@@ -18,17 +18,16 @@ def main():
         quit()
 
     if not archive.files:
-        common.index.index(archive)
+        config.index.index(archive)
     if not archive.files:
         return []
 
-    print(archive.subdirs[0].files)
-
-    archive_tree = mainWindow.CreateArchiveTreeView(archive)
-    archive_tree.show()
+    mainWindow = ui.UI()
+    mainWindow = mainWindow.CreateArchiveTreeView(archive)
+    mainWindow.show()
 
     sys.exit(app.exec())
 
 
 if __name__ == '__main__':
-    main()
+    main('/home/isa/Projects/plaguevfs/archives/Textures.vfs')
