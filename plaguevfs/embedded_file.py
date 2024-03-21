d246265dd9dd52c1a80b6e5aed34d36440607a92
isatsam <=>
2024-03-22 00:01:52 +0300

    Convert the file timestamp from uint64 to unix epoch

--- plaguevfs/embedded_file.py
+++ plaguevfs/embedded_file.py
@@ -1,5 +1,6 @@
 import codecs
 import struct
+from datetime import datetime
 
 
 class EmbeddedFile:
@@ -9,16 +10,30 @@ class EmbeddedFile:
 
     @staticmethod
     def read_self(section):
-        def read_int(data):
-            b = data.read(4)
-            return struct.unpack('<i', b)[0]
+        def to_datetime(filetime: int) -> datetime:
+            """
+            Converts a Windows filetime number to a Python datetime. The new
+            datetime object is timezone-naive but is equivalent to tzinfo=utc.
+
+            This function is adapted from winfiletime by jlenclanche:
+            https://github.com/jleclanche/winfiletime
+            which is released in the Public Domain.
+            """
+
+            # Get seconds and remainder in terms of Unix epoch
+            EPOCH_AS_FILETIME = 116444736000000000  # January 1, 1970 as filetime
+            HUNDREDS_OF_NS = 10000000
+            s, ns100 = divmod(filetime - EPOCH_AS_FILETIME, HUNDREDS_OF_NS)
+            # Convert to datetime object, with remainder as microseconds.
+            return datetime.utcfromtimestamp(s).replace(microsecond=(ns100 // 10))
 
         filename_len = ord(section.read(1))
         filename = section.read(filename_len)
-        length = read_int(section)
-        start = read_int(section)
+        length = struct.unpack('<i', section.read(4))[0]
+        start = struct.unpack('<i', section.read(4))[0]
         end = start+length
-        timestamp = section.read(8)
+        timestamp = struct.unpack('<q', section.read(8))[0]
+        timestamp = int(to_datetime(timestamp).strftime('%s'))
 
         return [filename, length, start, end, timestamp]
 
