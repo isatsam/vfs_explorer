b4d704bc3d8aa02b075c67dd30de4d34c6959879
isatsam <=>
2024-03-23 18:45:20 +0300

    Handle "no archive given" error when running vfs_toolkit as a module

--- vfs_toolkit/__main__.py
+++ vfs_toolkit/__main__.py
@@ -1,4 +1,9 @@
 import sys
 from .main import main
 
-main(sys.argv[1])
+try:
+    main(sys.argv[1])
+except IndexError:
+    print('Specify path to or filename of a VFS archive, like so:')
+    print('   python3 -m vfs_toolkit Example.vfs')
+    
