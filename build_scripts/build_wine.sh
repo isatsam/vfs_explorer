755e906e617c9caa71150c5df56b14ab6e811f5d
isatsam <=>
2024-05-20 02:15:45 +0300

    UI: Fix bugs and add a version file in build_wine.sh

--- build_scripts/build_wine.sh
+++ build_scripts/build_wine.sh
@@ -15,7 +15,7 @@
 # distro's repositories. If you don't need that functionality,
 # comment out the last two lines of the script.
 python_dir="C:/Python312"
-path_to_wineprefix = "`pwd`/wineprefix"
+path_to_wineprefix="`pwd`/wineprefix"
 
 program_name="VFS_Explorer_windows_amd64"
 
@@ -26,12 +26,56 @@ if [ -d "dist/$program_name.zip" ]; then
     rm -rf "dist/$program_name.zip";
 fi
 
+echo "# UTF-8
+#
+# For more details about fixed file info 'ffi' see:
+# http://msdn.microsoft.com/en-us/library/ms646997.aspx
+VSVersionInfo(
+  ffi=FixedFileInfo(
+# filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
+# Set not needed items to zero 0.
+filevers=(0, 1, 0, 0),
+prodvers=(0, 1, 0, 0),
+# Contains a bitmask that specifies the valid bits 'flags'r
+mask=0x3f,
+# Contains a bitmask that specifies the Boolean attributes of the file.
+flags=0x0,
+# The operating system for which this file was designed.
+# 0x4 - NT and there is no need to change it.
+OS=0x4,
+# The general type of file.
+# 0x1 - the file is an application.
+fileType=0x1,
+# The function of the file.
+# 0x0 - the function is not defined for this fileType
+subtype=0x0,
+# Creation date and time stamp.
+date=(0, 0)
+),
+  kids=[
+StringFileInfo(
+  [
+  StringTable(
+    u'040904B0',
+    [StringStruct(u'CompanyName', u'hypnotiger'),
+    StringStruct(u'FileDescription', u'Tool for VFS archives in IPL games'),
+    StringStruct(u'FileVersion', u'1.0.0'),
+    StringStruct(u'InternalName', u'VFS Explorer'),
+    StringStruct(u'LegalCopyright', u'Licensed under GNU GPLv3'),
+    StringStruct(u'OriginalFilename', u'VFS Explorer.exe'),
+    StringStruct(u'ProductName', u'VFS Explorer'),
+    StringStruct(u'ProductVersion', u'1.0.0')])
+  ]),
+  ]
+)" > version_file.txt
+
 WINEPREFIX="$path_to_wineprefix" wine $python_dir/Scripts/pyinstaller.exe \
-vfs_explorer.py
+vfs_explorer.py \
 --name "$program_name" \
 --contents-directory "programresources" \
 --noconsole \
---icon icon.ico
+--icon build_scripts/icon.ico \
+--version-file version_file.txt
 
-cd .dist/
-7za a $program_name.zip $program_name
+# cd dist/
+# 7za a $program_name.zip $program_name
